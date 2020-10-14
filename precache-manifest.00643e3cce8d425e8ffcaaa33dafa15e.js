ce87b29052be4cff6a50eaf2ea294833e0d055b3
unknown <snstotti@gmail.com>
2020-10-14 18:51:27 +0300

    Updates

---
+++ precache-manifest.00643e3cce8d425e8ffcaaa33dafa15e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9175568a1263263990eb7a69cee8c05f",
+    "url": "/test-app-adminka/index.html"
+  },
+  {
+    "revision": "e99162760815d2a72126",
+    "url": "/test-app-adminka/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "21dc84e4853be9a091c3",
+    "url": "/test-app-adminka/static/css/main.913df1ab.chunk.css"
+  },
+  {
+    "revision": "e99162760815d2a72126",
+    "url": "/test-app-adminka/static/js/2.72017079.chunk.js"
+  },
+  {
+    "revision": "a1a636632721a402612df3e043dd630d",
+    "url": "/test-app-adminka/static/js/2.72017079.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21dc84e4853be9a091c3",
+    "url": "/test-app-adminka/static/js/main.55f22d30.chunk.js"
+  },
+  {
+    "revision": "b6ae89cd27698b785340",
+    "url": "/test-app-adminka/static/js/runtime-main.52af7282.js"
+  }
+]);
